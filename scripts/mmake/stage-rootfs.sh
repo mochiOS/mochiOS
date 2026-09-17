6cc4a61af4c412c2a79fdafa85e6d206c60c53c0
tas0dev <156896971+tas0dev@users.noreply.github.com>
2026-09-17 17:13:08 +0900

    a

--- scripts/mmake/stage-rootfs.sh
+++ scripts/mmake/stage-rootfs.sh
@@ -58,6 +58,7 @@ stage_app() {
 stage_app "$root/applications/binder" Binder.app binder
 for resource in appicon.svg close.svg maximize.svg minimize.svg mochios.svg; do install -m 0644 "$root/applications/binder/resources/$resource" "$stage_new/applications/Binder.app/$resource"; done
 if [[ -d $root/applications/binder/resources/apps ]]; then cp -a "$root/applications/binder/resources/apps/." "$stage_new/applications/"; fi
+stage_app "$root/applications/appstore" AppStore.app appstore appicon.svg
 stage_app "$root/applications/test.app" test.app test_app
 stage_app "$root/applications/terminal" Terminal.app terminal appicon.svg
 stage_app "$root/applications/file" Files.app files appicon.svg
@@ -66,6 +67,7 @@ for resource in folder.svg file.svg application.svg image.svg archive.svg disk.s
 stage_app "$root/applications/settings" Settings.app settings appicon.png
 stage_app "$root/applications/installer" Installer.app installer appicon.svg
 install_manifest "$root/applications/binder/manifest.toml" binder
+install_manifest "$root/applications/appstore/manifest.toml" appstore
 install_manifest "$root/applications/terminal/manifest.toml" terminal
 install_manifest "$root/applications/file/manifest.toml" files
 install_manifest "$root/applications/settings/manifest.toml" settings
